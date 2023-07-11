f42e52f942dbd548b3a024ba2d6b16ee033ca7e1
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-07-11 13:21:47 +0530

    Add files via upload

---
+++ Notepad-main/Colour.java
@@ -0,0 +1,47 @@
+import java.awt.Color;
+
+public class Colour {
+    GUI gui;
+
+    public Colour(GUI gui){
+        this.gui = gui;
+    }
+
+    public void changeColour(String col){
+
+        switch (col) {
+
+            case "White": gui.window.getContentPane().setBackground(Color.white);
+                          gui.textArea.setBackground(Color.white);
+                          gui.textArea.setForeground(Color.black);
+                          break;
+
+            case "Black": gui.window.getContentPane().setBackground(Color.black);
+                          gui.textArea.setBackground(Color.black);
+                          gui.textArea.setForeground(Color.white);
+                          break;
+                          
+            case "Blue": gui.window.getContentPane().setBackground(Color.blue);
+                          gui.textArea.setBackground(Color.blue);
+                          gui.textArea.setForeground(Color.white);
+                          break;
+            case "Pink": gui.window.getContentPane().setBackground(Color.pink);
+                          gui.textArea.setBackground(Color.pink);
+                          gui.textArea.setForeground(Color.black);
+                          break;              
+            case "Yellow": gui.window.getContentPane().setBackground(Color.yellow);
+                          gui.textArea.setBackground(Color.yellow);
+                          gui.textArea.setForeground(Color.black);
+                          break; 
+            case "Orange": gui.window.getContentPane().setBackground(Color.orange);
+                          gui.textArea.setBackground(Color.orange);
+                          gui.textArea.setForeground(Color.black);
+                          break;   
+            case "Green": gui.window.getContentPane().setBackground(Color.green);
+                          gui.textArea.setBackground(Color.green);
+                          gui.textArea.setForeground(Color.black);
+                          break;                                     
+        }
+
+    }
+}
---
+++ Notepad-main/README.md
@@ -0,0 +1,30 @@
+# Notepad (TextEditor)
+Notepad is a simple notepad application: it can read, edit and create text files from the device storage or other apps.
+ ![image](https://user-images.githubusercontent.com/89564330/171341934-785b951b-0900-417c-9f79-651c934c33fd.png)  
+
+## Features
+
+- New , Open, edit and create text files , Save , SaveAs , Exit
+- Cursor and selection information
+- Undo actions
+- Cut , Copy , Paste
+- Text wrapping
+- Background Change 
+- Change Font Size
+- Change Font Family
+
+## Preview
+
+![image]()
+--> Java based Notepad with Stack type clipboard to keep multiple strins in clipboard\
+--> Graphical User Interface made by Java Swing\
+--> A menu bar with follwing menus
+ * 1. **File handling** (New , Open , Save, Save As , Exit),
+ * 2. **Editing options** (Cut , Copy , Paste)
+ * 3. **Formating of Fonts** with fontsize and font type with Word Wrap option
+ * 4. **Background colour** change
+
+ ## Author
+ ruchikayadav1408
+
+
